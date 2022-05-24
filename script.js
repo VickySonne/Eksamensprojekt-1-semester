cfb9ec499c8c7277924b6f8a6491b7fc8d3a45b0
VickySonne <vst1996@live.dk>
2022-05-24 17:46:20 +0200

    Udkommenterede Projekter og Ny Javascript

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function load(){
 }
 
 //Source: Sandie Neander Petersen
+//Gør at hjemmesiden starter ved forside sektionen.
+
+
+// Source: Christian Pedersen
+var item = document.getElementById("main");
+
+window.addEventListener("wheel", e => {
+    if (e.deltaY > 0) {
+        item.scrollLeft += 500;
+    } else {
+        item.scrollLeft -= 500;
+    }
+}); 
+
+//Det gør at brugeren kan scroll tilhøjre og venstre med musen.
